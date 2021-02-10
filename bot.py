9d028d5a9fb840af1b34c3f6795dc6ab79d99774
Bunny Pranav <pranav.vasipalli@gmail.com>
2021-02-10 09:33:11 +0530

    made inline false for all

--- bot.py
+++ bot.py
@@ -111,8 +111,8 @@ async def help(ctx):
     embed.add_field(
         name="rand", value="Use rand n1 n2 and number of random numbers *Ex. $rand 10 20*", inline=False)
     embed.add_field(
-        name="price", value="Get the price of a amazon product _Ex. $price (amazon url)_", inline=True)
-    embed.add_field(name="help", value="Shows this Message ", inline=True)
+        name="price", value="Get the price of a amazon product _Ex. $price (amazon url)_", inline=False)
+    embed.add_field(name="help", value="Shows this Message ", inline=False)
     embed.set_footer(text="A general purpose bot made by Bunny Pranav")
     await ctx.send(embed=embed)
 
